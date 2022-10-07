cc1be59706aefb2c28a762353aa23889aea56bc5
github_Ryan30551 <patrickryanf@gmail.com>
2022-10-07 01:14:41 -0500

    second commit

---
+++ pizzaorder.js
@@ -0,0 +1,21 @@
+function newOrder(){
+    let name = document.getElementById("customername").value;
+    let size = document.getElementById("pizzasize").value;
+    let toppings = document.getElementById("pizzatoppings").value;
+
+    let orders = document.getElementById("orders");
+    
+    //create a new row
+    let newRow = orders.insertRow(-1)
+    
+    let newNm = newRow.insertCell(0).appendChild(document.createTextNode(name));
+    let newSz = newRow.insertCell(1).appendChild(document.createTextNode(size));
+    let newTp = newRow.insertCell(2).appendChild(document.createTextNode(toppings));
+}
+
+let orderButton = document.getElementById("order-button");
+orderButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    newOrder();
+});
+
